32bfe67cb821d2ba0b6829a8bbf67e259226e9f6
medaymen007 <medaymen9m24@gmail.com>
2022-10-28 21:22:28 +0200

    upload the design

---
+++ sitePro.js
@@ -0,0 +1,18 @@
+let sectionOfDashboard = document.querySelector(".sectionOfDashboard");
+sectionOfDashboard.style.display = "none";
+let sectionOfSetting = document.querySelector(".sectionOfSetting");
+sectionOfSetting.style.display = "none";
+let sectionOfProfile = document.querySelector(".sectionOfProfile");
+sectionOfProfile.style.display = "none";
+let sectionOfProject = document.querySelector(".sectionOfProject")
+sectionOfProject.style.display = "none";
+
+
+
+let iconSpe = document.querySelector(".iconSpe");
+let btnSpe = document.querySelector(".speeee");
+//btnSpe.addEventListener("click", () => {
+//  iconSpe.style.justifyContent = "flex-start";
+//console.log("hello")
+//})
+console.log(btnSpe)
